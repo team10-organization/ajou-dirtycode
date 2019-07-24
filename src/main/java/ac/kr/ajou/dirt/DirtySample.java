e3e23278481ca1a3d4df8faa296b83bf5215dbd4
mistercle <mistercle@gmail.com>
2019-07-24 11:22:41 +0900

    refactor name check

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -12,23 +12,20 @@ public class DirtySample {
         for (int i = 0; i < items.length; i++)
         {
             checknameisnotBrieorBackstage(items[i]);
-//-------------------------------------------------------------------------------------------------------------------------
             lowerselllnfornotSulfuras(items[i]);
-//-------------------------------------------------------------------------------------------------------------------------
             isselllnminus(items[i]);
-//------------------------------------------------------------------------------------------------------------------------------
         }
     }
 
     private void isselllnminus(Item item) {
         if (item.sellIn < 0)
         {
-            if (!item.name.equals("Aged Brie"))
+            if (!isAged_brie(item))
             {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+                if (!isBackStage(item))
                 {
                     if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        if (!isSulfuras(item)) {
                             item.lowerquality();
                         }
                     }
@@ -49,11 +46,11 @@ public class DirtySample {
     }
 
     private void checknameisnotBrieorBackstage(Item item) {
-        if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+        if (!isAged_brie(item) && !isBackStage(item))
         {
             if (item.quality > 0)
             {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
+                if (!isSulfuras(item))
                 {
                     item.lowerquality();
                 }
@@ -66,7 +63,7 @@ public class DirtySample {
                 {
                     item.quality = item.quality + 1;
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+                    if (isBackStage(item))
                     {
                         if (item.sellIn < 11)
                         {
@@ -88,8 +85,21 @@ public class DirtySample {
             }
     }
 
+    private boolean isSulfuras(Item item) {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    private boolean isAged_brie(Item item) {
+        return item.name.equals("Aged Brie");
+    }
+
+    private boolean isBackStage(Item item)
+    {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
     private void lowerselllnfornotSulfuras(Item item) {
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
+        if (!isSulfuras(item))
         {
             item.lowersellln();
         }
