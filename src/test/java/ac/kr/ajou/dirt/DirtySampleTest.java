5452bc7f89081a970f153115bbd0ffe3a2e9c003
MaSungHo <tonem123@naver.com>
2019-07-24 13:05:30 +0900

    Add test methods by 마성호

--- src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
+++ src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
@@ -1,5 +1,6 @@
 package ac.kr.ajou.dirt;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
@@ -63,5 +64,55 @@ public class DirtySampleTest {
         dirtySample.updateQuality();
         assertThat(items[0].sellIn, is(0));
         assertThat(items[0].quality, is(1));
+    }
+
+    @Test
+    public void updateQuality_이름이_TAFKAL80ETC는_quality가_50이상이면_sellin만_1감소(){
+        Item[] items = {new Item(tafkal8oetc, 23, 54)
+                , new Item(tafkal8oetc, 64, 103)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+        Assert.assertTrue(items[0].quality == 54 && items[0].sellIn == 22);
+        Assert.assertTrue(items[1].quality == 103 && items[1].sellIn == 63);
+    }
+
+    @Test
+    public void updateQuality_이름이_TAFKAL80ETC는_quality가_49이면_quality_1증가_sellin_1감소(){
+        Item[] items = {new Item(tafkal8oetc, 23, 49)
+                , new Item(tafkal8oetc, 64, 49)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+        Assert.assertTrue(items[0].quality == 50 && items[0].sellIn == 22);
+        Assert.assertTrue(items[1].quality == 50 && items[1].sellIn == 63);
+    }
+
+    @Test
+    public void updateQuality_이름이_TAFKAL80ETC는_quality가_48이고_sellin이_1이상_11미만이면_quality_2증가_sellin_1감소(){
+        Item[] items = {new Item(tafkal8oetc, 1, 48)
+                , new Item(tafkal8oetc, 10, 48)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+        Assert.assertTrue(items[0].quality == 50 && items[0].sellIn == 0);
+        Assert.assertTrue(items[1].quality == 50 && items[1].sellIn == 9);
+    }
+
+    @Test
+    public void updateQuality_이름이_TAFKAL80ETC는_quality가_47이하_sellin이_1이상_6미만이면_quality_3증가_sellin_1감소(){
+        Item[] items = {new Item(tafkal8oetc, 1, 47)
+                , new Item(tafkal8oetc, 5, 12)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+        Assert.assertTrue(items[0].quality == 50 && items[0].sellIn == 0);
+        Assert.assertTrue(items[1].quality == 15 && items[1].sellIn == 4);
+    }
+
+    @Test
+    public void updateQuality_이름이_TAFKAL80ETC는_quality가_47이하_sellin이_6이상_11미만이면_quality_2증가_sellin_1감소(){
+        Item[] items = {new Item(tafkal8oetc, 6, 47)
+                , new Item(tafkal8oetc, 10, 12)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+        Assert.assertTrue(items[0].quality == 49 && items[0].sellIn == 5);
+        Assert.assertTrue(items[1].quality == 14 && items[1].sellIn == 9);
     }
 }
