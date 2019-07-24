3d3949b771061a80865503ee9f226522e55d4f5b
mistercle <mistercle@gmail.com>
2019-07-24 13:11:51 +0900

    refactored method name

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -12,13 +12,13 @@ public class DirtySample {
         for (int i = 0; i < items.length; i++)
         {
             if(isSulfuras(items[i])) continue;
-            else if(isAged_brie(items[i])) caseofbrie(items[i]);
-            else if(isBackStage(items[i])) caseofticket(items[i]);
-            else caseofremain(items[i]);
+            else if(isAged_brie(items[i])) updateBrie(items[i]);
+            else if(isBackStage(items[i])) updatePasses(items[i]);
+            else updateRemain(items[i]);
         }
     }
 
-    private void caseofremain(Item item)
+    private void updateRemain(Item item)
     {
         if(isZeroQuality(item)) item.lowerQuality();
         item.lowerSellIn();
@@ -28,11 +28,7 @@ public class DirtySample {
         }
     }
 
-    private boolean isZeroQuality(Item item) {
-        return item.quality > 0;
-    }
-
-    private void caseofticket(Item item)
+    private void updatePasses(Item item)
     {
 
         if(isNotMaxQuality(item))
@@ -56,11 +52,7 @@ public class DirtySample {
         if(item.sellIn < 0) item.zeroQuality();
     }
 
-    private boolean isNotMaxQuality(Item item) {
-        return item.quality < 50;
-    }
-
-    private void caseofbrie(Item item)
+    private void updateBrie(Item item)
     {
         if(isNotMaxQuality(item)) {
             item.higherQuality();
@@ -74,81 +66,6 @@ public class DirtySample {
 
     }
 
-//    private void updateQualitybyUniqueName(Item item) {
-//        if (!isAged_brie(item) && !isBackStage(item))
-//        {
-//            lowerQualityifPositiveQualityandNotSulfuras(item);
-//        }
-//
-//        else
-//        {
-//            if (item.quality < 50)
-//            {
-//                item.quality = item.quality + 1;
-//
-//                if (isBackStage(item))
-//                {
-//                    higherQualitybySellln(item);
-//                }
-//            }
-//        }
-//    }
-//
-//    private void lowerSelllnfornotSulfuras(Item item) {
-//        if (!isSulfuras(item))
-//        {
-//            item.lowerSellIn();
-//        }
-//    }
-//
-//    private void updateQualityForMinusSellln(Item item) {
-//        if (item.sellIn < 0)
-//        {
-//            if(isBackStage(item))
-//            {
-//                item.zeroQuality();
-//                return ;
-//            }
-//            if(isAged_brie(item))
-//            {
-//                higherQualitybelow50(item);
-//                return ;
-//            }
-//
-//            lowerQualityifPositiveQualityandNotSulfuras(item);
-//
-//
-//        }
-//    }
-//
-//    private void higherQualitybelow50(Item item) {
-//        if (item.quality < 50) {
-//            item.higherQuality();
-//        }
-//    }
-//
-//
-//
-//    private void higherQualitybySellln(Item item) {
-//        if (item.sellIn < 11)
-//        {
-//            higherQualitybelow50(item);
-//        }
-//
-//        if (item.sellIn < 6)
-//       {
-//           higherQualitybelow50(item);
-//       }
-//    }
-//
-//    private void lowerQualityifPositiveQualityandNotSulfuras(Item item) {
-//        if (item.quality > 0) {
-//            if (!isSulfuras(item)) {
-//                item.lowerQuality();
-//            }
-//        }
-//    }
-//
     private boolean isSulfuras(Item item) {
         return item.name.equals("Sulfuras, Hand of Ragnaros");
     }
@@ -162,6 +79,13 @@ public class DirtySample {
         return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
 
+    private boolean isNotMaxQuality(Item item) {
+        return item.quality < 50;
+    }
+
+    private boolean isZeroQuality(Item item) {
+        return item.quality > 0;
+    }
 
 
 }
